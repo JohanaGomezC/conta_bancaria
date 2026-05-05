bd376e13a93655754ac21df217bc08d7b24579bd
Johana Gomez <johagomez9806@gmail.com>
2026-05-05 19:25:43 -0300

    Commit CRUD

--- src/conta_bancaria/Menu.java
+++ src/conta_bancaria/Menu.java
@@ -3,6 +3,7 @@ package conta_bancaria;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import conta_bancaria.controller.ContaController;
 import conta_bancaria.model.Conta;
 import conta_bancaria.model.ContaCorrente;
 import conta_bancaria.model.ContaPoupanca;
@@ -13,26 +14,12 @@ public class Menu {
 	private static final Scanner leia = new Scanner(System.in);
 	// criando uma instancia unica imutavel de Scanner que só pode ser acessado na
 	// class Menu
+	private static final ContaController contaController = new ContaController();
+	
 
 	public static void main(String[] args) {
-
-		// Teste da Classe Conta Corrente
-
-		ContaCorrente cc1 = new ContaCorrente(2, 123, 1, "José da Silva", 0.0f, 1000.0f);
-		cc1.visualizar();
-		cc1.sacar(12000.0f);
-		cc1.visualizar();
-		cc1.depositar(5000.0f);
-		cc1.visualizar();
 		
-		// Teste da Classe Conta Poupança
-
-		ContaPoupanca cp1 = new ContaPoupanca(3, 123, 2, "Maria dos Santos", 100000.0f, 15);
-		cp1.visualizar();
-		cp1.sacar(1000.0f);
-		cp1.visualizar();
-		cp1.depositar(5000.0f);
-		cp1.visualizar();
+		criarContasTeste();
 
 		int opcao;
 
@@ -77,10 +64,16 @@ public class Menu {
 			switch (opcao) {
 			case 1:
 				System.out.println(Cores.TEXT_WHITE_BOLD + "Criando conta\n\n");
+				
+				cadastrarConta();
+				
 				keyPress();
 				break;
 			case 2:
 				System.out.println(Cores.TEXT_WHITE_BOLD + "Listar todas as contas\n\n");
+				
+				listarContas();
+				
 				keyPress();
 				break;
 			case 3:
@@ -122,6 +115,52 @@ public class Menu {
 		System.out.println("Johana Gómez Cardona - johagomez9806@gmail.com");
 		System.out.println("https://github.com/JohanaGomezC");
 		System.out.println("*****************************************************");
+	}
+	
+	private static void listarContas() {
+		contaController.listarTodas();
+	}
+	
+	private static void cadastrarConta() {
+		
+		System.out.print("Digite o número da Agência: ");
+		int agencia = leia.nextInt();
+		
+		System.out.print("Digite o nome do Titular: ");
+		leia.skip("\\R");
+		String titular = leia.nextLine();
+		
+		System.out.print("Digite o tipo da conta (1 - CC | 2 - CP): ");
+		int tipo = leia.nextInt();
+		
+		System.out.print("Digite o Saldo incial da conta: ");
+		float saldo = leia.nextFloat();
+		
+		switch (tipo) {
+		case 1 -> {
+			System.out.println("Digite o limite da conta: ");
+			float limite = leia.nextFloat();
+			contaController.cadastrar(new ContaCorrente(contaController.gerarNumero(), agencia, tipo, titular, saldo, limite));
+		}
+		case 2 -> {
+			System.out.println("Digite o dia do aniversário da conta: ");
+			int aniversario = leia.nextInt();
+			contaController.cadastrar(new ContaPoupanca(contaController.gerarNumero(), agencia, tipo, titular, saldo, aniversario));	
+		}
+		
+		default -> System.out.println(Cores.TEXT_RED_BOLD + "Tipo de conta inválida!" + Cores.TEXT_RESET);		
+		}
+	}
+	
+	private static void criarContasTeste() {
+		contaController.cadastrar(
+				new ContaCorrente( contaController.gerarNumero(), 123, 1, "João da Silva", 1000.00f, 100.00f));
+		contaController.cadastrar(
+				new ContaCorrente( contaController.gerarNumero(), 456, 1, "Maria dos Santos", 2000.00f, 200.00f));
+		contaController.cadastrar(
+				new ContaCorrente( contaController.gerarNumero(), 789, 2, "Mariana Hernandez", 10000.00f, 12));
+		contaController.cadastrar(
+				new ContaCorrente( contaController.gerarNumero(), 123, 2, "Giovanna Giunchetti", 8000.00f, 23));
 	}
 	
 	public static void keyPress() {
